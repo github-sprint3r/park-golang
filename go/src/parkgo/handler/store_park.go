114a2df9b757c4d1641eb05d1cea1d5784a6c7a3
Man Proteus <man@proteus-tech.com>
2014-07-06 13:09:08 +0700

    Add ParkStore base

---
+++ go/src/parkgo/handler/store_park.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type ChargeRate struct {
+	Rate string `json:"rate"`
+	Value int `json:"value"`
+}
+
+func ParkStore(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, `{"chargeRates":[{"rate":"r1","value":0}]}`)
+}
